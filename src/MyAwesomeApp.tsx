4a23e3ac708d957e00e7a0476afa853322c979fa
Agustin <agusgibaut693@gmail.com>
2025-09-04 14:03:18 -0300

    impresion de variables

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -5,12 +5,21 @@ export function MyAwesomeApp () {
 
        //arreglos
        const games = ['crash', 'mario', 'pes6']
+       const activo = 'true';
+
+       //objetos literales
+       const direccion = {
+        codigo: 'abc-123',
+        Country: 'new york',
+       }
 
     return (
         <>
         <h1>  {nombre} </h1>
         <h1>  {apellido}  </h1>
         <p> {games.join(', ')} </p>
+        <h1> {activo ? 'activo': 'no activo'}  </h1>
+        <p> { JSON.stringify(direccion)} </p>
         </>
     )
 }
